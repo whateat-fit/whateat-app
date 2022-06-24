75c23275ac32fc090a6e155eb09968a6203b2319
playerony <pawel.wojtasinski.1995@gmail.com>
2022-06-24 19:13:26 +0200

    chore: configure tailwind

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
   import logo from './assets/svelte.png';
   import Counter from './lib/Counter.svelte';
+  import TailwindCss from './TailwindCSS.svelte';
 </script>
 
-<main>
-  <img src="{logo}" alt="Svelte Logo" />
-  <h1>Hello Typescript!</h1>
+<main class="flex items-center justify-center p-4 flex-col">
+  <img class="h-40 w-40 mx-auto" src="{logo}" alt="Svelte Logo" />
+  <h1 class="text-[#ff3e00] uppercase text-6xl my-8 mx-auto max-w-[14rem]">Hello Typescript!</h1>
+  <TailwindCss />
   <Counter />
-  <p>
+  <p class="max-w-[14rem] my-4 mx-auto">
     Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.
   </p>
-  <p>
+  <p class="max-w-[14rem] my-4 mx-auto">
     Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for the officially supported
     framework, also powered by Vite!
   </p>
@@ -20,33 +22,6 @@
   :root {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell,
       'Open Sans', 'Helvetica Neue', sans-serif;
-  }
-
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
-  img {
-    height: 16rem;
-    width: 16rem;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4rem;
-    font-weight: 100;
-    line-height: 1.1;
-    margin: 2rem auto;
-    max-width: 14rem;
-  }
-
-  p {
-    max-width: 14rem;
-    margin: 1rem auto;
-    line-height: 1.35;
   }
 
   @media (min-width: 480px) {
