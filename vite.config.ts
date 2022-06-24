7b00ea128459d5ed7e25f48a5ea37439f90cfbeb
playerony <pawel.wojtasinski.1995@gmail.com>
2022-06-24 19:38:04 +0200

    chore: configure sass to fix warning with global styles

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import postcss from './postcss.config.cjs';
+import sveltePreprocess from 'svelte-preprocess';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte({
+      preprocess: sveltePreprocess(),
+    }),
+  ],
   css: {
     postcss,
   },
