d35f4918c5ebed65d62f8bf7daaa9f75ca8c6f1c
playerony <pawel.wojtasinski.1995@gmail.com>
2022-06-24 15:28:51 +0200

    chore: update

--- src/App.svelte
+++ src/App.svelte
@@ -6,13 +6,10 @@
 <main>
   <img src="{logo}" alt="Svelte Logo" />
   <h1>Hello Typescript!</h1>
-
   <Counter />
-
   <p>
     Visit <a href="https://svelte.dev">svelte.dev</a> to learn how to build Svelte apps.
   </p>
-
   <p>
     Check out <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a> for the officially supported
     framework, also powered by Vite!
